2185daa958a5e83556ef9dc4fe4ca407e4243472
Chris Mizikakis <cmizikakis@gmail.com>
2020-12-26 00:00:51 +0200

    Add IIPInfoProviderService

--- Implementation/Common/IPInfoProvider.Interfaces/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace IPInfoProvider.Interfaces
-{
-    public class Class1
-    {
-    }
-}
---
+++ Implementation/Common/IPInfoProvider.Interfaces/IIPInfoProviderService.cs
@@ -0,0 +1,10 @@
+﻿using IPInfoProvider.Types.Models;
+using System;
+
+namespace IPInfoProvider.Interfaces
+{
+    public interface IIPInfoProviderService
+    {
+        IPDetails GetDetails(string ip);
+    }
+}
