fb0fa576d9a31c9bebff0960211dad3fcaa9c3f7
Chris Mizikakis <cmizikakis@gmail.com>
2020-12-26 02:27:35 +0200

    Implement BuildHttpRequestMessage method

--- Implementation/Common/IPInfoProvider.Helpers/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace IPInfoProvider.Helpers
-{
-    public class Class1
-    {
-    }
-}
---
+++ Implementation/Common/IPInfoProvider.Helpers/Helper.cs
@@ -0,0 +1,18 @@
+﻿using IPInfoProvider.Types.Models;
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace IPInfoProvider.Helpers
+{
+    public static class Helper
+    {
+        public static void BuildHttpRequestMessage(HttpRequestMessage request, string ip, string baseUrl, string accessKey)
+        {
+            request.RequestUri = new Uri(baseUrl + ip + "?" + accessKey);
+            request.Headers
+                  .Accept
+                  .Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+    }
+}
